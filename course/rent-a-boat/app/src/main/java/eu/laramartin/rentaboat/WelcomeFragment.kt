b2bed03ee5c05901f13afc8db6500d0e620f491d
laramartin <laramartin@users.noreply.github.com>
2019-01-11 15:57:38 +0100

    find nav controller to navigate

--- course/rent-a-boat/app/src/main/java/eu/laramartin/rentaboat/WelcomeFragment.kt
+++ course/rent-a-boat/app/src/main/java/eu/laramartin/rentaboat/WelcomeFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
 class WelcomeFragment : Fragment() {
 
@@ -18,12 +20,16 @@ class WelcomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_welcome, container, false)
 
+        view.findViewById<Button>(R.id.button_login).setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                findNavController().navigate(R.id.action_welcomeFragment_to_homeFragment)
+            }
+        }
+
         view.findViewById<Button>(R.id.button_registration).setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_registrationFragment)
-        )
-
-        view.findViewById<Button>(R.id.button_login).setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_homeFragment)
         )
 
         return view
