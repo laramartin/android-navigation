a3a486b2507d6a5f79cb0ba9bdcf4c7dd2c037e9
laramartin <laramartin@users.noreply.github.com>
2019-01-11 13:35:01 +0100

    Navigation from welcome fragment to registration fragment via onClickListener

--- course/rent-a-boat/app/src/main/java/eu/laramartin/rentaboat/WelcomeFragment.kt
+++ course/rent-a-boat/app/src/main/java/eu/laramartin/rentaboat/WelcomeFragment.kt
@@ -6,17 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class WelcomeFragment : Fragment() {
 
     override fun onCreateView(
@@ -24,8 +16,12 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+
+        view.findViewById<Button>(R.id.button_registration).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_registrationFragment)
+        )
+
+        return view
     }
-
-
 }
