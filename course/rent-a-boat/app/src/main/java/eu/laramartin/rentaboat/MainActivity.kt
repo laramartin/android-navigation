9e930ffeeca31f3ebcaae7f42c3ee010b369cc51
laramartin <laramartin@users.noreply.github.com>
2019-01-11 12:46:14 +0100

    Setup project

---
+++ course/rent-a-boat/app/src/main/java/eu/laramartin/rentaboat/MainActivity.kt
@@ -0,0 +1,12 @@
+package eu.laramartin.rentaboat
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
