cd2d5aa4faa90b7e1533e0afa9a601f9bd6802d3
laramartin <laramartin@users.noreply.github.com>
2019-01-24 14:43:52 +0100

    Navigate to home and profile via bottom nav bar

--- navigation-playground/app/src/main/java/eu/laramartin/navigationplayground/MainActivity.kt
+++ navigation-playground/app/src/main/java/eu/laramartin/navigationplayground/MainActivity.kt
@@ -3,18 +3,24 @@ package eu.laramartin.navigationplayground
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment
+//
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+            .setupWithNavController(navController = navHostFragment.navController)
 
-        bottomNavigationView = findViewById(R.id.bottom_nav_view)
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
 
-
+            supportActionBar!!.title = destination.label
+        }
     }
 
 }
